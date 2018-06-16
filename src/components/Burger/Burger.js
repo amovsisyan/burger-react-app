b3dedcce3dcbf4a8864cd6f41e4fdd60f6f3e849
art <amovsisyan0@gmail.com>
2018-06-16 23:30:05 +0400

    added burger component and burgerbuilder container without controllers

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+
+import classes from './Burger.css'
+
+const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingredKey => {
+            return [...Array(props.ingredients[ingredKey])]
+                .map((_, index) => {
+                    return (
+                        <BurgerIngredient
+                            key={ingredKey + index}
+                            type={ingredKey}
+                        />
+                    )
+                });
+        });
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type={'bread-top'}></BurgerIngredient>
+            {transformedIngredients}
+            <BurgerIngredient type={'bread-bottom'}></BurgerIngredient>
+        </div>
+    );
+};
+
+export default burger;
